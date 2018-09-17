82c2407ec78abefb17c3f4f5d7a56a186c87bf81
Mario Kramer <kramer.mario@ymail.com>
2018-09-17 14:35:14 +0200

    Initial commit. Spring-Initializr

---
+++ src/main/java/de/mariokramer/sspChallenge/SspChallengeApplication.java
@@ -0,0 +1,12 @@
+package de.mariokramer.sspChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SspChallengeApplication.class, args);
+	}
+}
