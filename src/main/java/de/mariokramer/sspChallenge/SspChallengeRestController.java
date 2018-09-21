3746209b6af43d4a79ffb977cc63696327b5212a
Mario Kramer <kramer.mario@ymail.com>
2018-09-21 19:48:42 +0200

    index.html angelegt mit href. Template soll folgen

--- src/main/java/de/mariokramer/sspChallenge/SspChallengeRestController.java
+++ src/main/java/de/mariokramer/sspChallenge/SspChallengeRestController.java
@@ -6,7 +6,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("newGame")
